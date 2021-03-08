673e04a3eec3bd9311301a0ce2085e012c2a90cd
mansukim1125 <mansukim1125@gmail.com>
2021-03-08 17:29:00 +0900

    Add home title design

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,44 @@
 
 import AbstractComponent from './Abstract';
 
+import '../css/Home.css';
+
 export default class HomeComponent extends AbstractComponent {
     mount() {
         this.mountPosId = 'main-container';
+        this.interval = null;
+        this.titleText = '일상의 문제를 해결하는 서비스를 만듭니다'
         super.mount();
+        this.writeInterval(this.interval);
+    }
+    writeInterval(interval) {
+        this.title = document.getElementById('main-title');
+        let i = 0;
+        interval = setInterval(() => {
+            i++;
+            if (i >= this.titleText.length) {
+                clearInterval(interval);
+                this.borderInterval();
+            }
+            this.title.innerText = this.titleText.substr(0, i);
+        }, 150);
+    }
+    borderInterval() {
+        let i = 0;
+        setInterval(() => {
+            i++;
+            if (i % 2) {
+                this.title.style.borderRight = '0';
+            } else {
+                this.title.style.borderRight = '1px solid';
+            }
+        }, 450);
     }
     getHTML() {
         return `
-            <h1 id="main-title">
-                일상의 문제를 해결하는 서비스를 만듭니다
-            </h1>
+            <div id="container">
+                <h1 id="main-title"></h1>
+            </div>
         `;
     }
 }
